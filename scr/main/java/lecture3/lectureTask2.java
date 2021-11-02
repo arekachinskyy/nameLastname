aa8fe7cf349525def45114e8e3852b59ade9c7e3
arekachinskyy <andrey.rekachinskyy@gmail.com>
2021-11-02 23:13:49 +0200

    [+] Added homework (Random Numbers generator, Riddle with one question and 3 rounds)

---
+++ scr/main/java/lecture3/lectureTask2.java
@@ -0,0 +1,17 @@
+package lecture3;
+
+import java.util.Random;
+//HOMEWORK: RANDOM NUMBERS GENERATOR
+public class lectureTask2 {
+    public static void main(String[] args) {
+        int[] arrNumber = {15, 8, 24, 41, 9, 84};
+        Random random = new Random();
+        int randomNumber = random.nextInt(arrNumber.length);
+
+        if (arrNumber[randomNumber] > 10){
+            System.out.print("I am lucky");
+        } else {
+            System.out.print("Run again");
+        }
+    }
+}
